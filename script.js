36f5cf0840a4c9991451505e59dfdd8c0f5db980
Your Name <nbos0531@gmail.com>
2018-06-11 20:54:33 -0500

    submit button added

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+//Button Options-------------------------------
+var food;
+var quality;
+var time;
 //Choose Food Option
 $('.food').on('click', function() {
+	food = $(this).val();
+	console.log(food);
 	$('#second').css('display', 'block');
 	$('#first').css('display', 'none');
 });
 
 //Choose Cheap Second Card
-$('#Cheap').on('click', function() {
+$('.q2').on('click', function() {
+	quality = $(this).val();
+	console.log(quality);
 	$('#second').css('display', 'none');
 	$('#third').css('display', 'block');
 });
 
-//Choose Healthy Second Card
-$('#Healthy').on('click', function() {
-	$('#second').css('display', 'none');
-	$('#third').css('display', 'block');
-});
 
 //Go Back To First Card
 $('#goBack').on('click', function() {
@@ -26,5 +29,24 @@ $('#goBack').on('click', function() {
 $('#goBack2').on('click', function() {
 	$('#second').css('display', 'block');
 	$('#third').css('display', 'none');
-})
+});
 
+//Go Back To Third Card
+$('#goBack3').on('click', function() {
+	$('#fourth').css('display', 'none');
+	$('#third').css('display', 'block');
+});
+
+//Time Card Choice Option
+$('.t2').on('click', function() {
+	time = $(this).val();
+	console.log(time);
+	$('#fourth').css('display', 'block');
+	$('#third').css('display', 'none');
+	$('#foodChoice').html(food);
+	$('#foodQuality').html(quality);
+	$('#foodTime').html(time);
+});
+
+
+//Choice Recording-----------------------------
