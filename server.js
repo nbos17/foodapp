670373fa407f2211b07bdba799d1f5ab65b427df
Your Name <nbos0531@gmail.com>
2018-06-17 12:16:25 -0500

    server running

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var public = path.join(__dirname, 'public');
+var mongoose = require("mongoose");
+
+var PORT = process.env.PORT || 3000;
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(public, 'index.html'));
 });
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/foodapp";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 app.use('/', express.static(public));
 
-app.listen(8080);
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
