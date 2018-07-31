ebef95528584035f57ebfdb52af915abbcd482cc
nbos17 <nbos0531@gmail.com>
2018-07-30 23:19:30 -0500

    better get day function

--- public/assets/script.js
+++ public/assets/script.js
@@ -154,23 +154,20 @@ $('#random').on('click', function() {
 
     //choose restaurant
 
-      var today = new Date();
-      var dd = today.getDate();
-      var mm = today.getMonth()+1; //January is 0!
-      var yyyy = today.getFullYear();
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] =  "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
 
-      if(dd<10) {
-          dd = '0'+dd
-      } 
+    var n = weekday[d.getDay()];
+    console.log(n);
 
-      if(mm<10) {
-          mm = '0'+mm
-      } 
-
-      today = mm + '/' + dd + '/' + yyyy;
-      console.log(today);
-
-    if (today == '07/31/2018') {
+    if (n == 'Tuesday') {
       eatHere = response[5].name;
       menu = 'BUCKETS!!!';
        //display correct card
